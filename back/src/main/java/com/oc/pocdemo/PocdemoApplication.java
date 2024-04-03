e149b199dd2d9049cc9ae624c2fef32657d8c9db
Stephane <stephane.hua1997@gmail.com>
2024-04-04 00:24:12 +0200

    Poc chatbox

---
+++ back/src/main/java/com/oc/pocdemo/PocdemoApplication.java
@@ -0,0 +1,13 @@
+package com.oc.pocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocdemoApplication.class, args);
+	}
+
+}
